f1a5c3c2456e3f98c44bc027c30768d2c378b3f0
itachi1706 <kennethsohyq@hotmail.com>
2015-08-09 17:33:25 +0800

    Added default recyclerview sample lol

--- app/src/main/java/com/itachi1706/shoppingtracker/MainActivityFragment.java
+++ app/src/main/java/com/itachi1706/shoppingtracker/MainActivityFragment.java
@@ -2,9 +2,17 @@ package com.itachi1706.shoppingtracker;
 
 import android.support.v4.app.Fragment;
 import android.os.Bundle;
+import android.support.v7.widget.DefaultItemAnimator;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.GestureDetector;
 import android.view.LayoutInflater;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.LinearLayout;
+
+import com.itachi1706.shoppingtracker.Adapters.StringRecyclerAdapter;
 
 /**
  * A placeholder fragment containing a simple view.
@@ -16,10 +24,26 @@ public class MainActivityFragment extends Fragment {
 
     View v;
 
+    RecyclerView recyclerView;
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         v = inflater.inflate(R.layout.fragment_main_screen, container, false);
+
+        String[] tmp = {"A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P"};
+
+
+        recyclerView = (RecyclerView) v.findViewById(R.id.rv_items);
+        recyclerView.setHasFixedSize(true);
+        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(getActivity());
+        linearLayoutManager.setOrientation(LinearLayoutManager.VERTICAL);
+        recyclerView.setLayoutManager(linearLayoutManager);
+        StringRecyclerAdapter adapter = new StringRecyclerAdapter(tmp);
+        recyclerView.setAdapter(adapter);
+        recyclerView.setItemAnimator(new DefaultItemAnimator());
+
+
         return v;
     }
 }
